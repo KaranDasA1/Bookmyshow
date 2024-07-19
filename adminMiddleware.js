f6705c3010820f318f2e8ca57a9c3a33151b5901
KaranDasA1 <141057770+KaranDasA1@users.noreply.github.com>
2024-07-19 15:31:13 +0530

    Delete adminMiddleware.js

--- adminMiddleware.js
+++
@@ -1,9 +0,0 @@
-const adminMiddleware = (req, res, next) => {
-    if (req.body.user.role !== "admin") {
-      return res.send({
-        success: false,
-        message: "You are not authorized to perform this action!",
-      });
-    }
-    next();
-  };
