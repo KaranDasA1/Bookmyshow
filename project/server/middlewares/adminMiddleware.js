ece71a8912610a62587cb9d09240ee3ec4eef4d0
KaranDasA1 <141057770+KaranDasA1@users.noreply.github.com>
2024-07-19 15:32:03 +0530

    Create adminMiddleware.js

---
+++ project/server/middlewares/adminMiddleware.js
@@ -0,0 +1,10 @@
+
+const adminMiddleware = (req, res, next) => {
+    if (req.body.user.role !== "admin") {
+      return res.send({
+        success: false,
+        message: "You are not authorized to perform this action!",
+      });
+    }
+    next();
+  };
